fc2e725324f286b621f4a2d7327dfebc96bbd8d0
Mikko <mijuraut@gmail.com>
2022-12-22 15:19:23 +0200

    alaku

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import requests
+
+APPLICATION_ID = "31111960"
+APPLICATION_KEY = "a080bc2fc55f5f7f74023e418f45609c"
+
+header = {
+    "id": "123"
+}
+
+# response = requests(header= , json= , )
 print("Hello")
